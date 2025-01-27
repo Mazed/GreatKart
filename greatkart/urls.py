cc7b3644b791ff555a979b2fa8b972d492900fc5
Mazed <raiyoz@live.com>
2025-01-27 13:48:25 +0700

    review and rating system

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -3,14 +3,12 @@ from django.urls import path, include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
-from store.views import search
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
-    path('search/', search, name='search'),
     path('accounts/', include('accounts.urls')),
     path('orders/', include('orders.urls')),
 ]
