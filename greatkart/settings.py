a5de9575327520aa31fa521a4edfd53e54811f51
Mazed <raiyoz@live.com>
2025-01-29 05:04:23 +0700

    final

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY='django-insecure-9mt+@$kkwb&4(hccpdr5@g^s)wd^+7idw14r-z%(6sl-eebya&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -160,8 +160,8 @@ MESSAGE_TAGS = {
 
 # SMTP configuration
 
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT', cast=int)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER='delilas444@gmail.com'
+EMAIL_HOST_PASSWORD='coey ydzl qjpy rytc'
+EMAIL_USE_TLS=True
